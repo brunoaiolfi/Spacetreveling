0d13cbdfd8962070b1e32ce1e004dce6b1f7c8f7
Bruno Aiolfi <bruno.aiolfi154@gmail.com>
2023-01-18 18:56:28 -0300

    Add files via upload

--- src/components/PostResume/index.tsx
+++ src/components/PostResume/index.tsx
@@ -1,24 +1,21 @@
 import { Post } from "../../models/interface/Post";
 import Link from 'next/link';
 import { formatDate } from "../../utils/dates";
+import styles from "./postResume.module.scss"
+import Info from "../Info";
 
 export function PostResume({ data, first_publication_date, uid }: Post) {
     return (
         <Link href={`/posts/${uid}`} key={uid}>
-            <a key={uid}>
-                <strong>{data.title}</strong>
-                <p>{data.subtitle}</p>
-                <footer>
-                    <section>
-                        <span>
-                            {formatDate(first_publication_date)}
-                        </span>
-                    </section>
-                    <section>
-                        <span>
-                            {data.author}
-                        </span>
-                    </section>
+            <a key={uid} className={styles.container}>
+                <strong className={styles.title}>{data.title}</strong>
+                <p className={styles.description}>{data.subtitle}</p>
+                <footer className={styles.footer}>
+                    <Info
+                        image="calendar"
+                        text={formatDate(first_publication_date)}
+                    />
+                    <Info image="user" text={data.author} />
                 </footer>
             </a>
         </Link>
